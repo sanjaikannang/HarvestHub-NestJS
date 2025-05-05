fa0fb43c54effbd1058688c78a95a9846ef1e19f
Sanjai Kannan G <136103561+sanjaikannang@users.noreply.github.com>
2025-05-05 22:26:56 +0530

    added DB schema and created base project structure and wrote dummy API for register and login

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,35 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { UserModule } from './user/user.module';
+import { AdminModule } from './admin/admin.module';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+
+    ConfigModule.forRoot({
+      isGlobal: true
+    }),
+
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: () => {
+        return {
+          uri: process.env.MONGODB_URL,
+          connectionFactory: () => {
+          },
+        };
+      },
+    }),
+
+    AuthModule,
+    UserModule,
+    AdminModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
